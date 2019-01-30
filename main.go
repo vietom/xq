2592b86e200281c77c1ba3c7ed83909cb27a6371
vietom <vietom@gmail.com>
2019-01-30 21:54:56 +0400

    updates

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 // xq . xmlfile ...
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -12,25 +13,41 @@ import (
 )
 
 func main() {
-	file := flag.String("i", "config.xml", "input file")
+	raw := flag.Bool("r", false, "raw output instead of xml")
 	flag.Parse()
-	if len(flag.Args()) < 1 {
-		fmt.Println("Usage:\txq [-i FILE] SELECTOR\nor:\techo '<xml>...</xml>' | xq SELECTOR")
-		os.Exit(1)
-	}
-	selector := flag.Args()[0]
-
-	doc := etree.NewDocument()
-	if err := doc.ReadFromFile(*file); err != nil {
+	info, err := os.Stdin.Stat()
+	if err != nil {
 		panic(err)
 	}
 
-	subdoc := etree.NewDocument()
-	for _, e := range doc.FindElements(selector) {
-		//fmt.Printf("%s: %s\n", e.Tag, e.Text())
-		subdoc.AddChild(e)
+	if len(flag.Args()) < 1 || info.Mode()&os.ModeCharDevice != 0 {
+		fmt.Println("Usage echo '<xml>...</xml>' | xq OPERATION")
+		os.Exit(1)
+	}
+	reader := bufio.NewReader(os.Stdin)
+
+	selector := flag.Args()[0]
+
+	doc := etree.NewDocument()
+	if _, err := doc.ReadFrom(reader); err != nil {
+		panic(err)
 	}
 
-	subdoc.Indent(2)
-	subdoc.WriteTo(os.Stdout)
+	values := make([]string, 0)
+	subdoc := etree.NewDocument()
+	for _, e := range doc.FindElements(selector) {
+		subdoc.AddChild(e)
+		if len(e.Text()) > 0 {
+			values = append(values, e.Text())
+		}
+	}
+
+	if *raw {
+		for _, e := range values {
+			fmt.Println(e)
+		}
+	} else {
+		subdoc.Indent(2)
+		subdoc.WriteTo(os.Stdout)
+	}
 }
